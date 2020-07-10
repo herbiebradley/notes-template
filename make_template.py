ec1e5298d2effe0621b38d8415b7f9579fc8f0b0
Herbie Bradley <mail@herbiebradley.com>
2020-07-10 18:18:50 +0100

    Fix parsing, add requirements

--- make_template.py
+++ make_template.py
@@ -1,18 +1,18 @@
-"""Code adapted from https://gist.github.com/neighthan/e7d088ab752ece583ffc16178ac0d9b4"""
+"""Code adapted from https://gist.github.com/neighthan/e7d088ab752ece583ffc16178ac0d9b4."""
 import requests
 from datetime import datetime
 from raschietto import Raschietto, Matcher
 from argparse import ArgumentParser
 from os.path import realpath, join, exists
 from email.utils import parsedate
-from typing import Dict
 
 
-def main() -> None:
+def main():
     parser = ArgumentParser()
     parser.add_argument("url")
     parser.add_argument("short_title", help="Short title for paper; used to name file.")
-    parser.add_argument("-j", "--journal", help="Journal published in [default = arXiv if 'arxiv' in url else '']", default="")
+    parser.add_argument("-j", "--journal", help="Journal published in [default = arXiv if 'arxiv' in url else '']",
+                        default="")
     args = parser.parse_args()
 
     journal = "arXiv" if "arxiv" in args.url and not args.journal else args.journal
@@ -26,11 +26,8 @@ def main() -> None:
     if "arxiv" in args.url:
         paper_data.update(extract_arxiv_data(args.url))
     else:
-        response = requests.get(args.url)
-        html = BeautifulSoup(response.content, "lxml")
-
         paper_data.update({
-            "title": str(html.select_one("h1").string),
+            "title": "",
             "authors": "[]",
             "year": int(paper_data["added_date"].split("-")[0]),
             "arxiv_id": "",
@@ -54,23 +51,23 @@ def main() -> None:
         f.write(template)
 
 
-def extract_arxiv_data(url: str) -> Dict[str, str]:
+def extract_arxiv_data(url):
     paper_data = {}
 
     page = Raschietto.from_url(url)
 
-    title_matcher = Matcher("#abs > div.leftcolumn > h1",
-                        mapping=lambda element, page: Raschietto.element_to_text(element)[6:])
-    authors_matcher = Matcher("#abs > div.leftcolumn > div.authors",
-                        mapping=lambda element, page: Raschietto.element_to_text(element)[8:].split(", "))
-    year_matcher = Matcher("#abs > div.leftcolumn > div.dateline",
-                        mapping=lambda element, page: Raschietto.element_to_text(element)[16:])
+    title_matcher = Matcher("#abs > h1",
+                            mapping=lambda element, page: Raschietto.element_to_text(element)[6:])
+    authors_matcher = Matcher("#abs > div.authors",
+                              mapping=lambda element, page: Raschietto.element_to_text(element)[8:].split(", "))
+    year_matcher = Matcher("#abs > div.dateline",
+                           mapping=lambda element, page: Raschietto.element_to_text(element))
 
     paper_data["title"] = title_matcher(page)
     paper_data["authors"] = authors_matcher(page)
 
     year = year_matcher(page)
-    paper_data["year"] = year[year.find("20"):year.find("20") + 4]
+    paper_data["year"] = year[year.rfind("20"):year.rfind("20") + 4]
 
     paper_data["arxiv_id"] = url[url.find("abs") + 4:]
     return paper_data
